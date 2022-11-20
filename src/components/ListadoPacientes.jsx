26de875017e286272a90860c4fb7f80645761216
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2022-11-20 19:44:24 -0300

    mostrar titulos con condicionales

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -3,17 +3,29 @@ import Paciente from './Paciente'
 const ListadoPacientes = ({ pacientes }) => {
   return (
     <div className='md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll'>
-      <h2 className='font-black text-xl text-center'>Listado Pacientes</h2>
-      <p className='text-xl mt-5 mb-10 text-center'>
-        Administra tus{' '}
-        <span className='text-indigo-600 font-bold'>Pacientes y Citas</span>
-      </p>
-      {pacientes.map(paciente => (
-        <Paciente
-          key={paciente.id} // se le pasa el id unico igual para el servidor y el cliente
-          paciente={paciente} // se le pasa el paciente
-        />
-      ))}
+      {pacientes && pacientes.length ? (
+        <>
+          <h2 className='font-black text-3xl text-center'>Listado Pacientes</h2>
+          <p className='text-lg mt-5 mb-10 text-center'>
+            Administra tus{' '}
+            <span className='text-indigo-600 font-bold'>Pacientes y Citas</span>
+          </p>
+          {pacientes.map(paciente => (
+            <Paciente
+              key={paciente.id} // se le pasa el id unico igual para el servidor y el cliente
+              paciente={paciente} // se le pasa el paciente
+            />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className='font-black text-3xl text-center'>No hay pacientes</h2>
+          <p className='text-lg mt-5 mb-10 text-center'>
+            Comienza agregando pacientes{' '}
+            <span className='text-indigo-600 font-bold'>y los verás aquí</span>
+          </p>
+        </>
+      )}
     </div>
   )
 }
